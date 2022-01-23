bf6caad75f9de3ffc1395b5895eeff82181c023c
dominikklucka <Squawkers@users.noreply.github.com>
2022-01-23 20:41:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
-
+/*
 // Add your functions below:
 const validateCred = (array) => {
   
@@ -111,3 +111,97 @@ const idInvalidCardCompanies = (cardNumArr) => {
 };
 
 idInvalidCardCompanies(batch);
+*/
+
+// validate credit card
+const validateCred = (creditArr) =>{
+
+  let reverseArr = creditArr.reverse(); // reverse arr 
+  let len = creditArr.length - 1; // getting card number length (16)
+  let sum = 0;
+
+  for (let i = 0; i < creditArr.length; i++){
+    
+    let calculatedAmount =reverseArr[i];
+    
+    // every other digit is doubled 
+    if(i !== 0 && i%2 === 0){
+
+      calculatedAmount = reverseArr[i] * 2;
+
+      // if the doubled amount it gt 9
+      if(calculatedAmount > 9){
+         calculatedAmount = calculatedAmount - 9;
+         sum += calculatedAmount;
+      } else {
+        sum += calculatedAmount;
+      }
+    } else {
+       sum += reverseArr[i];
+    }
+  }
+  return(sum%10 === 0 ? true : false);
+}
+
+// validating multiple cards
+const findInvalidCards = (arr) =>{
+  
+  const falseCards = [];
+  
+  // grabbing nested arrays
+  for (let i = 0; i < arr.length; i++){
+    
+    let creditCard = arr[i];
+    if((validateCred(creditCard) === false)){
+         falseCards.push(arr[i]);
+    }
+  }
+  return (falseCards);
+}
+
+// identifying companies that issued faulty numbers
+const idInvalidCardCompanies = arr =>{
+  
+  let firstNums = [];
+  
+  // getting first numbers from all nested credit cards
+  for (let i = 0; i < arr.length; i++){
+    let oriArr= arr[i].reverse(); // reverse arr back
+    firstNums.push(oriArr[0]);
+  }
+  
+  // getting unique values
+  const uniqSet = new Set(firstNums);
+  const back2Arr = [...uniqSet];
+  
+  // print out companies that mailed out invalid credit card numbers
+  for (let i = 0; i < back2Arr.length; i++) {
+
+    switch (back2Arr[i]){
+      case 3:
+        console.log("Amex");
+        break;
+      case 4:
+        console.log("Visa");
+        break;
+      case 5:
+        console.log("Mastercard");
+        break;
+      case 6:
+        console.log("Discover");
+        break;
+      default:
+        console.log("Company not found.");
+    }
+  }
+}
+
+// question 7.
+const formattingString = str => {
+   let creditCard = str.split("");
+  console.log(creditCard);
+}
+
+// *************************************** MAIN
+idInvalidCardCompanies(findInvalidCards(batch));
+formattingString("1234567");
